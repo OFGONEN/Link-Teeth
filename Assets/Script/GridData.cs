e6fbde716fd11aaa7d865fddb01b03b27d357388
ofgonen <ofgonen@gmail.com>
2022-02-07 18:14:50 +0300

    GridData: Visualization on Inspector

--- Assets/Script/GridData.cs
+++ Assets/Script/GridData.cs
@@ -5,37 +5,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
 
 [ CreateAssetMenu( fileName = "grid_data_", menuName = "FF/Game/GridData" ) ]
-public class GridData : ScriptableObject
+public class GridData : SerializedScriptableObject
 {
-	public int size_row;
-	public int size_column;
-	public GridToothData[] gridToothData;
+	[ TitleGroup( "Data"), TableMatrix( DrawElementMethod = "DrawElement" ) ] public GridToothData[,] gridToothData;
 
 #if UNITY_EDITOR
-	[ Button() ]
-	public void CreateGridArray()
+	static GridToothData DrawElement( Rect rect, GridToothData data )
 	{
-		if( size_row >= 1 && size_column >= 1)
-			gridToothData = new GridToothData[ size_row * size_column ];
-		else
-			FFLogger.LogError( "Both Row and Column sizes needs to be bigger than 1." );
-	}
+		data.tooth_color = SirenixEditorFields.ColorField( rect.SubXMax( 100 ), data.tooth_color );
+		data.tooth_type = ( ToothType )SirenixEditorFields.EnumDropdown( rect.AddXMin( 100 ), data.tooth_type );
 
-	[ Button() ]
-	public void SetData( int row, int column, GridToothData data )
-	{
-		if( data.tooth_type == ToothType.None )
-			FFLogger.LogError( "Cannot Select NONE as Tooth Type!!" );
-		else if( row >= 0 && row <= size_row && column >= 0 && column <= size_column )
-		{
-			var index    		   = row * size_row + column;
-			data.tooth_color 	   = data.tooth_color.SetAlpha( 1 );
-			gridToothData[ index ] = data;
-		}
-		else
-			FFLogger.LogError( "Select correct index for the Grid" );
+		return data;
 	}
 #endif
 }
