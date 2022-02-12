2400637f2b15fb7eca5a140b6caeb4460ae9016f
Ömer Faruk Gönen <ofgonen@gmail.com>
2022-02-12 23:04:32 +0300

    Line: Implement API methods

--- Assets/Script/Line.cs
+++ Assets/Script/Line.cs
@@ -3,10 +3,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
 
 public class Line : MonoBehaviour
 {
 #region Fields
+    [ BoxGroup( "Setup" ) ] public LinePool linePool;
+    [ BoxGroup( "Setup" ) ] public LineRenderer lineRenderer;
 #endregion
 
 #region Properties
@@ -16,6 +20,42 @@ public class Line : MonoBehaviour
 #endregion
 
 #region API
+    public void Spawn( Vector3 position, Color color )
+    {
+		gameObject.SetActive( true );
+
+		lineRenderer.startWidth = GameSettings.Instance.line_width;
+		lineRenderer.endWidth   = GameSettings.Instance.line_width;
+
+		lineRenderer.positionCount = 2;
+		lineRenderer.SetPosition( 0, position );
+		lineRenderer.SetPosition( 1, position );
+
+		lineRenderer.startColor = color;
+		lineRenderer.endColor   = color;
+	}
+
+    public void DeSpawn()
+    {
+		lineRenderer.positionCount = 0;
+		linePool.ReturnEntity( this );
+	}
+
+    public void AppendPoint( Vector3 position )
+    {
+		lineRenderer.positionCount += 1;
+		UpdatePoint( position );
+	}
+
+    public void RemovePoint()
+    {
+		lineRenderer.positionCount -= 1;
+	}
+    
+    public void UpdatePoint( Vector3 position )
+    {
+		lineRenderer.SetPosition( lineRenderer.positionCount - 1, position );
+    }
 #endregion
 
 #region Implementation
