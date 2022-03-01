19cfa3e030c5bf6655b3d1a1849bbb7663488890
ofgonen <ofgonen@gmail.com>
2022-03-01 14:48:35 +0300

    PalateTest: OnDrawGizmos

--- Assets/Test/PalateTest.cs
+++ Assets/Test/PalateTest.cs
@@ -3,6 +3,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using FFStudio;
 
 public class PalateTest : MonoBehaviour
@@ -42,6 +43,29 @@ public class PalateTest : MonoBehaviour
 
 #region Editor Only
 #if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        if( Application.isPlaying )
+        {
+			var tooth_data_array = palateData.palateToothData;
+
+            GUIStyle style = new GUIStyle { normal = new GUIStyleState { textColor = Color.black }, fontSize = 10 };
+
+			for( var i = 0; i < tooth_data_array.Length; i++ )
+			{
+				var data = tooth_data_array[ i ];
+				var tooth = transform.GetChild( data.tooth_index );
+
+				var position = tooth.position + Vector3.up * 0.05f;
+
+				Handles.color = data.tooth_color;
+				Handles.Label( position, "Tooth: " + data.tooth_type.ToString() + "\nHealth " + data.tooth_health, style );
+
+				Handles.color = Color.black;
+				Handles.DrawLine( tooth.position, position );
+			}
+		}
+    }
 #endif
 #endregion
 }
